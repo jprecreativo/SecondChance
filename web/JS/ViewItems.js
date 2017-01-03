4381dff90a2a0789741f9b71439de6d5ffe20277
jose_ <jose_@LAPTOP-J364L24S>
2017-01-03 16:51:48 +0100

    Versión 0.2.1. Add filterButton onclick function.

--- web/JS/ViewItems.js
+++ web/JS/ViewItems.js
@@ -5,3 +5,33 @@ noFilterButton.onclick = function() {
     
     window.location.reload(false);
 };
+
+var filterButton = document.getElementById("filterButton");
+
+filterButton.onclick = function() {
+    
+    var xhr;
+    
+    if(window.XMLHttpRequest)   // Navegadores actuales.
+        xhr = new XMLHttpRequest();
+    
+    else if(window.ActiveXObject)   // Para versiones antiguas de IE.
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    
+    if (xhr == null)
+        alert ("Your browser does not support AJAX!");
+        
+    else
+    {
+        var url = "ViewItems.jsp";
+        
+        xhr.onreadystatechange = getResponse;
+        xhr.open("GET", url, false);
+        xhr.send(null);
+    }
+};
+
+function getResponse()
+{
+    
+}
